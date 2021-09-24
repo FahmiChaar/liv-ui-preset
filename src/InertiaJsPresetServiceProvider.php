883c299f8268ff59672b425d64bfcb53dace83e9
fahmi <fahmichaar@gmail.com>
2021-09-24 21:42:18 +0100

    original package

---
+++ src/InertiaJsPresetServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaravelFrontendPresets\InertiaJsPreset;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Ui\UiCommand;
+
+class InertiaJsPresetServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        UiCommand::macro('inertiajs', function ($command) {
+            InertiaJsPreset::install();
+
+            $command->info('Inertia.js scaffolding installed successfully.');
+            $command->info('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
+        });
+    }
+}

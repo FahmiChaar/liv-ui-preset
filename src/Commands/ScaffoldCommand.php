732a243d3e6a8e60dec967d92848153a5e546ca1
fahmi <fahmichaar@gmail.com>
2022-04-08 01:41:59 +0100

    Infyom views refactoring

--- src/Commands/ScaffoldCommand.php
+++ src/Commands/ScaffoldCommand.php
@@ -4,9 +4,25 @@ namespace Liv\InertiaJsPreset\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+use Illuminate\Filesystem\Filesystem;
 
 class ScaffoldCommand extends Command
 {
+
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        parent::__construct();
+        $this->filesystem = $filesystem;
+    }
+
+
     /**
      * The name and signature of the console command.
      *
@@ -42,5 +58,30 @@ class ScaffoldCommand extends Command
             '--tableName' => $tableName,
             '--factory' => true,
         ]);
+        $this->refactoringInfyomViews($modelPlural);
     }
+
+    private function refactoringInfyomViews($model) {
+        $modePageDirectory = resource_path('js/Pages/'.$model);
+        $createFile = $this->getFileContent($model, 'create');
+        $fields = $this->getFileContent($model, 'fields');
+        $file = str_replace('$FIELDS$', $fields, $createFile);
+        $createPath = $modePageDirectory.'/create.blade.php';
+        $this->filesystem->put($createPath, $file);
+        $this->filesystem->move($createPath, $modePageDirectory.'/Create.vue');
+        $this->filesystem->move($modePageDirectory.'/index.blade.php', $modePageDirectory.'/Index.vue');
+        $allBladeFile = $this->filesystem->glob(resource_path('js/Pages/'.$model.'/*.blade.php'));
+        $this->filesystem->delete($allBladeFile);
+        $this->info('Infyom views refactoring completed');
+    }
+
+    private function getFileContent($modelName, $viewName) {
+        $directoryPath = resource_path('js/Pages/'.$modelName.'/');
+        try {
+            return $this->filesystem->get($directoryPath.$viewName.'.blade.php');
+        } catch (\Exception $e) {
+            return $this->error('File not found => '. $e->getMessage());
+        }
+    }
+
 }

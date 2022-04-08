7f907bc21c9f855488e85b10568accf3855b31d3
fahmi <fahmichaar@gmail.com>
2022-04-08 03:22:07 +0100

    PublishCommand

---
+++ src/Commands/PublishCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Liv\InertiaJsPreset\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class PublishCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'liv:publish';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish the UI scaffolding to infyom folder';
+
+    /**
+     * Execute the console command.
+     * @return mixed
+     */
+    public function handle()
+    {
+        (new Filesystem)->copyDirectory(__DIR__.'/inertiajs-stubs/resources/infyom', resource_path('infyom'));
+        $this->info('UI scaffolding published successfully.');
+    }
+}

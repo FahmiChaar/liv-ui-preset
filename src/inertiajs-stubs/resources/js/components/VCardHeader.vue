7da669f53e54406338dfe8dab8e6bfd1bfdf979e
fahmi <fahmichaar@gmail.com>
2022-04-12 17:39:56 +0100

    card header showCloseModal prop

--- src/inertiajs-stubs/resources/js/components/VCardHeader.vue
+++ src/inertiajs-stubs/resources/js/components/VCardHeader.vue
@@ -10,12 +10,15 @@
                 {{btnText}} <v-icon v-if="btnIcon" right dark>{{btnIcon}}</v-icon>
             </v-btn>
             <slot></slot>
+            <v-btn icon color="white" text @click="$modal.close()" v-if="showCloseModal">
+                <v-icon>close</v-icon>
+            </v-btn>
         </v-toolbar>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['color', 'titleClass', 'title', 'href', 'btnColor', 'btnClass', 'btnText', 'btnIcon']
+    props: ['color', 'titleClass', 'title', 'href', 'btnColor', 'btnClass', 'btnText', 'btnIcon', 'showCloseModal']
 }
 </script>
